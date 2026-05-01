443756a010f8cf61f895d4977337ecedff4f10e0
mtv2665 <  GitHub>
2026-05-01 03:58:02 -0700

    Clean build and fix config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// تأكد أن التصدير هو default ويرجع كائن الإعدادات عبر defineConfig
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // المجلد اللي Vercel بيتوقع يلقى فيه الملفات الجاهزة
-  }
+    outDir: 'dist',
+  },
+  base: '/',
 })
